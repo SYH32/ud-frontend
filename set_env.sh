4f02baac35f3f8acb28b0afcd0311a571a1dff52
Saud AlHamidi <s.alhamiedie@takamol.com.sa>
2020-09-17 16:19:09 +0300

    update set_env file

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=
-export POSTGRES_PASSWORD=
-export POSTGRES_HOST=
-export POSTGRES_DB=
-export AWS_BUCKET=
-export AWS_REGION=
-export AWS_PROFILE=
-export JWT_SECRET=
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=Post12345
+export POSTGRES_HOST=database-1.cxk9phxeaixy.eu-west-3.rds.amazonaws.com
+export POSTGRES_DB=postgress_dev
+export AWS_BUCKET=ucnd-dev
+export AWS_REGION=eu-west-3
+export AWS_PROFILE=defult
+export JWT_SECRET=helloworld
 export URL=http://localhost:8100
