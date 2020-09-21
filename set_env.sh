8e248c5d4b936dcec01dcf1a50c24f03e91b1a37
Saud AlHamidi <s.alhamiedie@takamol.com.sa>
2020-09-21 16:40:50 +0300

    .

--- set_env.sh
+++ set_env.sh
@@ -6,6 +6,6 @@ export POSTGRES_HOST=database-1.cxk9phxeaixy.eu-west-3.rds.amazonaws.com
 export POSTGRES_DB=postgress_dev
 export AWS_BUCKET=ucnd-dev
 export AWS_REGION=eu-west-3
-export AWS_PROFILE=defult
+export AWS_PROFILE=default
 export JWT_SECRET=helloworld
 export URL=http://localhost:8100
